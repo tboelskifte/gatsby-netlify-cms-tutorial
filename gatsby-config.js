4421d24a5ecf5dabf0344e1e75e72993ea79a9a8
Thomas Boelskifte <thomasb@me.com>
2019-05-31 00:28:02 +0200

    Added processing of markdown files
    Followed https://www.gatsbyjs.org/docs/adding-markdown-pages/

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,30 @@
 /**
- * Configure your Gatsby site with this file.
+ *  Configure your Gatsby site with this file.
  *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ *  See: https://www.gatsbyjs.org/docs/gatsby-config/
+ *
+ *  Se her for info om de enkelte plug-ins:
+ *     https://www.gatsbyjs.org/docs/adding-markdown-pages/
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+
+  // Installerede plug-ins:
+  plugins: [
+
+    { // Read files into Gatsby from the filesystem
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: "markdown-pages",
+      },
+    },
+
+    // Netlify plugin
+    `gatsby-plugin-netlify-cms`,
+
+    // Transforming markdown: Will recognise files which are markdown and read their content. Will convert the frontmatter metadata part of your markdown file as frontmatter and the content part as HTML.
+    `gatsby-transformer-remark`,
+
+  ]
 }
